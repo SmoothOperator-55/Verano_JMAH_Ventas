1d5e23f50dede83c2df619d50e055dae863449b3
Jose Alvarado <mostiproo@outlook.com>
2024-06-13 12:16:58 -0600

    Version02. DataContext y base de datos

---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+      
+        [Display (Name= "Pais")]
+        [MaxLength(100, ErrorMessage ="El campo debe de tener un maximo de 1 caracteres")]
+        [Required(ErrorMessage ="El campo es obligatorio")]
+
+        public string Name { get; set; } = null;
+
+
+      
+    }
+}
