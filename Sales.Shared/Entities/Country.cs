643e74ba6e7fc838568610f7d70d67cfaeddfe6b
Jose Alvarado <mostiproo@outlook.com>
2024-06-17 02:20:42 -0600

    P04 Componentes, crear países, alertas SweetAlert2

--- Sales.Shared/Entities/Country.cs
+++ Sales.Shared/Entities/Country.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
